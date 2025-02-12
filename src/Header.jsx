53eb6508a097befeb641d5a684385acc28b68858
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-02-12 13:13:40 -0500

    Ajustement style

--- src/Header.jsx
+++ src/Header.jsx
@@ -24,11 +24,11 @@ const Header = () => {
 
                 <nav className={menuOpen ? "menu-open" : ""}>
                     <ul className="menuListStyle">
-                        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-                        <li><Link to="/about" onClick={closeMenu}>À propos</Link></li>
-                        <li><Link to="/qualifications" onClick={closeMenu}>Qualifications</Link></li>
-                        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
-                        <li><Link to="/rdv" onClick={closeMenu}>Rendez-vous</Link></li>
+                        <li><Link to="/" onClick={() => setMenuOpen(false)}>{t("home")}</Link></li>
+                        <li><Link to="/about" onClick={() => setMenuOpen(false)}>{t("A_propos")}</Link></li>
+                        <li><Link to="/qualifications" onClick={() => setMenuOpen(false)}>Qualifications</Link></li>
+                        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+                        <li><Link to="/rdv" onClick={() => setMenuOpen(false)}>{t("rdv")}</Link></li>
                     </ul>
                 </nav>
 

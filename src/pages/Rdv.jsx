abb207f4681ed0213ef4c62dd8cc18be9395fa86
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-04-25 16:41:29 -0400

    arranger rbq

--- src/pages/Rdv.jsx
+++ src/pages/Rdv.jsx
@@ -121,7 +121,7 @@ const Rdv = () => {
             </form>
             <div className="info-box">
                 <h3>Besoin d'aller plus loin ?</h3>
-                <p>Si vous souhaitez faire une soumission, rendez-vous sur notre site <strong>app-soumission</strong> ou téléchargez l'application <strong>App-Soumission</strong>.</p>
+                <p>Si vous souhaitez faire une soumission, rendez-vous sur notre site <strong>Bâti Québec Soumissions</strong> ou téléchargez l'application <strong>Bâti Québec Soumissions</strong>.</p>
                 <p>Pour estimer les prix de vos projets, téléchargez l'application <strong>Bâti Québec Estimation</strong>.</p>
             </div>
         </div>

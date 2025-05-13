39a4d9a87936cd59e4de5f57098128a6e72609f2
MyriamMontmo <2047939@cmontmorency.qc.ca>
2025-05-13 15:05:52 -0400

    image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
                     position: "relative"
                 }}>
                     <img
-                        src="./images/reno.jpg"
+                        src="./images/pexels-tima-miroshnichenko-6615230.jpg"
                         alt="Background"
                         style={{
                             width: "100%",
